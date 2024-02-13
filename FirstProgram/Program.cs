77691d4751157f178de9f26cb83870621b2884a4
zilionn <correiafernando165@gmail.com>
2024-02-13 18:11:58 -0300

    First project in c#

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        for (int i = 0; i < 5; i++) {
+            int x = int.Parse(Console.ReadLine());
+            if (x == 5 || x ==0) {
+                Console.WriteLine("Pls, seja mais criativo");
+            } else {
+                Console.WriteLine("Ok, computando...");
+            }
+            Console.WriteLine("Valor é: " +x);
+
+        }
+    }
+}
